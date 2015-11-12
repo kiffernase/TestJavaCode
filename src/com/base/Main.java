88cebd527ad989d2ad5b5b9aa4376dd57784b136
kiffernase <m_schmidbauer@web.de>
2015-11-12 08:54:52 +0100

    Desktop Commit

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -3,7 +3,10 @@ package com.base;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    // Connection to Repository is : OK
+	/*
+    write your code here
+    Connection to Repository is : OK
+    and fully usable
+    */
     }
 }
