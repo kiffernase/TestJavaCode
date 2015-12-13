ec99e287e0ffc94f3ba82a5381a8c1653d99d604
kiffernase <m_schmidbauer@web.de>
2015-12-13 12:18:51 +0100

    Desktop commit

--- src/com/base/Main.java
+++ src/com/base/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	/*
     write your code here
     Connection to Repository is : OK
-    and fully usable
+    and fully usable tested on Desktop
     */
         System.out.println("Hello World!");
     }
